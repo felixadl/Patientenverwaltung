1e9cf46f0616f84cc9b98497aed4b4768fa19943
felixadl <felix-adler@gmx.de>
2015-05-07 09:05:31 +0200

    Controller Class

---
+++ src/patientenverwaltung/Controller.java
@@ -0,0 +1,11 @@
+package patientenverwaltung;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+
+    public List<Patient> patienten = new ArrayList<Patient>();
+
+
+}
