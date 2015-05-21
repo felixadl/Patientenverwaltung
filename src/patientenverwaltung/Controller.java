e801b7683433f3f500f713e4015b82d3e3238799
lorenzadl <lorenz-adler@gmx.de>
2015-05-21 08:31:41 +0200

    added Autom_fuellen()

--- src/patientenverwaltung/Controller.java
+++ src/patientenverwaltung/Controller.java
@@ -1,4 +1,7 @@
 package patientenverwaltung;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +14,7 @@ public class Controller {
     public Controller() {
 
         this.patienten = new ArrayList<Patient>();
+        Autom_fuellen();
 
     }
 
@@ -24,6 +28,25 @@ public class Controller {
         System.out.println("Neuer Patient: " + Vorname + " " + Nachname + ", geboren am " + Geburtstag + ", wohnhaft in " + Adresse + ", " + Wohnort + ".");
 
     }
+
+    public void Autom_fuellen() {
+        PrintWriter pWriter=null;
+        try{
+            pWriter = new PrintWriter(new FileWriter("patienten.txt",true),true);
+            pWriter.println("Adler"  +" " + "Felix"+ " " + "22.02.98.1998" + " " + "Am Kirschgarten 1" + " " + "94127 Neuburg/Inn");
+            pWriter.println("Adler"  +" " + "Lorenz"+ " " + "22.02.98.1998" + " " +"Am Kirschgarten 1" + " " + "94127 Neuburg/Inn");
+            pWriter.println("Wik"  +" " + "Julian"+ " " + "29.08.1997" + " " + "Brunnfeld 14" + " " + "94127 Neuburg/Inn");
+            pWriter.println("Ellinger"  +" " + "Jonas"+ " " + "22.01.1998" + " " + "Heiningerstrasse 44a" + " " + "94036 Passau");
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }finally {
+            if(pWriter != null){
+                pWriter.flush();
+                pWriter.close();
+            }
+        }
+    }
+
 
 
 }
